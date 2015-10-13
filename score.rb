0565eef65de90a13c48748052d8e6e516aff7ab5
James Washington <jwash1987@gmail.com>
2015-10-13 18:28:31 -0400

    enumerable part 1

---
+++ score.rb
@@ -0,0 +1,13 @@
+scores = [83, 71, 92, 64, 98, 87, 75, 69]
+
+high_scores = scores.select { |n| n > 80}
+
+p high_scores
+
+low_scores = scores.reject { |n| n > 80 }
+
+p low_scores
+
+puts scores.any? { |n| n < 70}
+
+puts scores.detect { |n| n < 70}
